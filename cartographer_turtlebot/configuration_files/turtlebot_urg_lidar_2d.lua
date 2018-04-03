7da09f5e6b930932e389ac6bc0d7af53169f0933
Christof Dubs <christof.dubs@gmx.ch>
2018-04-03 15:02:04 +0900

    improve local SLAM config

--- cartographer_turtlebot/configuration_files/turtlebot_urg_lidar_2d.lua
+++ cartographer_turtlebot/configuration_files/turtlebot_urg_lidar_2d.lua
@@ -48,8 +48,10 @@ TRAJECTORY_BUILDER_2D.min_range = 0.1
 TRAJECTORY_BUILDER_2D.max_range = 8.
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 5.
 TRAJECTORY_BUILDER_2D.use_imu_data = true
-TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
-TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
+TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = false
+TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(1.)
+TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.1
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 10
 
 POSE_GRAPH.constraint_builder.min_score = 0.65
 POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
